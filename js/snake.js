7c94931e6da98442e14e251c3c341f3a4443c66a
Chad Cunningham <chadcunningham7676@gmail.com>
2022-05-07 14:23:27 -0400

    Added Coord to snake

--- js/snake.js
+++ js/snake.js
@@ -1,9 +1,13 @@
+const Coord = require('./coord')
+
 class Snake {
   constructor(board) {
     this.dir = "N";
     this.board = board;
-    
-    const center = [Math.floor(board.dim / 2), Math.floor(board.dim / 2)];
-    this.segments = center;
+
+    const center = new Coord(Math.floor(board.dim / 2), Math.floor(board.dim / 2));
+    this.segments = [center];
   }
 }
+
+module.exports = Snake;
